69f0fc0abb4b8a44bfc443e63d8dde3da26b4da9
kce22 <co.e@husky.neu.edu>
2020-03-26 19:36:58 -0400

    fixed async routes, added route files

---
+++ src/controllers/studentCourseRoutes.js
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const studentcourses = await req.context.models.StudentCourse.findAll();
+        return res.send(studentcourses);
+    } catch(e) {
+        console.log(e);
+        return res.send(e);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const studentcourse = await req.context.models.StudentCourse.create(req.body);
+        return res.send(studentcourse);
+    } catch (e) {
+        console.log(e);
+        return res.send(e);
+    }
+});
+
+router.put('/:nuid/:courseID', async (req, res) => {
+    try {
+        const studentcourse = await req.context.models.StudentCourse
+            .updateStudentCourse(req.params.NUID, req.params.courseID, req.body);
+        return res.send(studentcourse);
+    } catch (e) {
+        console.log(e);
+        return res.send(e);
+    }
+});
