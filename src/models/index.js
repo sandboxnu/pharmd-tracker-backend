6ee10e932ff7c43abf8968e4ef19deaf42399001
sophiacrennan <crennan.s@husky.neu.edu>
2020-03-26 15:42:47 -0700

    Fix associations for assessments and notes

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ import {Sequelize} from 'sequelize';
 require('dotenv').config();
 const sequelize = new Sequelize(
 
-    'postgres',
+    process.env.DB,
 
     process.env.DB_USERNAME,
 
@@ -25,20 +25,37 @@ const models = {
 
     StudentAssessment: sequelize.import('./studentassessment'),
 
+  //  CourseAssessment: sequelize.import('./courseassessment'),
+
     Student: sequelize.import('./student'),
 
     Course: sequelize.import('./course'),
 
     Assessment: sequelize.import('./assessment'),
 
+    Note: sequelize.import('./note'),
+
+//    StudentNote: sequelize.import('./studentnote'),
+
 };
 
+// StudentCourse Association
 models.Student.belongsToMany(models.Course, {through: models.StudentCourse, foreignKey:'NUID', sourceKey:'NUID'});
 models.Course.belongsToMany(models.Student, {through: models.StudentCourse, foreignKey:'courseID', sourceKey:'courseID'});
+
+// StudentAssessment Association
 models.Student.belongsToMany(models.Assessment, {through: models.StudentAssessment, foreignKey:'NUID',
 sourceKey:'NUID'});
 models.Assessment.belongsToMany(models.Student, {through: models.StudentAssessment, foreignKey:'assessmentID',
 sourceKey:'assessmentID'});
+
+// // CourseAssessment Association
+models.Assessment.belongsTo(models.Course);
+models.Course.hasMany(models.Assessment);
+//
+// // StudentNote Association
+models.Note.belongsTo(models.Student);
+models.Student.hasMany(models.Note);
 
 
 Object.keys(models).forEach(key => {
