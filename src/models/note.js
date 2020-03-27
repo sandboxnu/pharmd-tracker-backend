85f5fdc663b4dcbe1862ab60eb6c7b4d69107ba1
sophiacrennan <crennan.s@husky.neu.edu>
2020-03-26 17:39:16 -0700

    Fix pcf model/npm error

--- src/models/note.js
+++ src/models/note.js
@@ -1,38 +1,32 @@
 // TODO notes should have tags, date, title, body
-
-const note = (sequelize, DataTypes) => {
-    const Note = sequelize.define('note', {
-        noteID: {
-            type: DataTypes.STRING,
-            unique: true,
-            primaryKey: true
-        },
-        NUID: { // this should be a foreign key
-            type: DataTypes.STRING,
-            unique: false,
-            primaryKey: true
-        },
-        date: {
-            type: DataTypes.DATE,
-            unique: false,
-            allowNull: false
-        },
-        title: {
-            type: DataTypes.STRING,
-            unique: false,
-            allowNull: false
-        },
-        body: {
-            type: DataTypes.STRING,
-            unique: false,
-            allowNull: false
-        },
-        tags: {
-            type: DataTypes.ARRAY(DataTypes.STRING),
-            unique: false,
-            allowNull: true
-        }
-    });
-
-    return Note;
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Note = sequelize.define('note', {
+    noteID: {
+      type: DataTypes.STRING,
+      unique: true,
+      primaryKey: true
+    },
+    date: {
+      type: DataTypes.DATE,
+      unique: false,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      unique: false,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      unique: false,
+      allowNull: false
+    },
+    tags: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      unique: false,
+      allowNull: true
+    }
+  });
+  return Note;
 };
--- src/models/pcf.js
+++ src/models/pcf.js
@@ -1,39 +1,41 @@
-const pcf = (sequelize, DataTypes) => {
-    const PCF = sequelize.define('pcf', {
-        NUID: {
-            type: DataTypes.STRING,
-            unique: true,
-            primaryKey: true
-        },
-        submissionDate: {
-            type: DataTypes.DATE,
-            unique: false,
-            allowNull: false
-        },
-        submittedBy: {
-            type: DataTypes.STRING,
-            unique: false,
-            allowNull: false
-        },
-        type: {
-            type: DataTypes.ENUM('Portfolio', 'Classroom', 'IPPE', 'APPE'),
-            unique: false,
-            allowNull: false
-        },
-        reviewedBy: {
-            type: DataTypes.ENUM('PCF Only', 'APCB', 'ASC'),
-            unique: false,
-            allowNull: false
-        },
-        meetingDate: {
-            type: DataTypes.DATE,
-            unique: false,
-            allowNull: true
-        },
-        decisionRendered: {
-            type: DataTypes.STRING,
-            unique: false,
-            allowNull: true
-        }
-    });
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const pcf = sequelize.define('pcf', {
+    pcfID: {
+      type: DataTypes.STRING,
+      unique: true,
+      primaryKey: true
+    },
+    submissionDate: {
+      type: DataTypes.DATE,
+      unique: false,
+      allowNull: false
+    },
+    submittedBy: {
+      type: DataTypes.STRING,
+      unique: false,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.ENUM('Portfolio', 'Classroom', 'IPPE', 'APPE'),
+      unique: false,
+      allowNull: false
+    },
+    reviewedBy: {
+      type: DataTypes.ENUM('PCF Only', 'APCB', 'ASC'),
+      unique: false,
+      allowNull: false
+    },
+    meetingDate: {
+      type: DataTypes.DATE,
+      unique: false,
+      allowNull: true
+    },
+    decisionRendered: {
+      type: DataTypes.STRING,
+      unique: false,
+      allowNull: true
+    }
+  });
+  return pcf;
 };
