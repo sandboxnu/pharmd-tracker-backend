45b11c19f14083a5bad7f8c2f8c00971d1100f4b
kce22 <co.e@husky.neu.edu>
2020-04-12 16:47:51 -0400

    fixed indentation

--- src/models/pcf.js
+++ src/models/pcf.js
@@ -1,46 +1,46 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const PCF = sequelize.define('pcf', {
-    pcfID: {
-      type: DataTypes.STRING,
-      unique: true,
-      primaryKey: true
-    },
-    submissionDate: {
-      type: DataTypes.DATE,
-      unique: false,
-      allowNull: false
-    },
-    submittedBy: {
-      type: DataTypes.STRING,
-      unique: false,
-      allowNull: false
-    },
-    type: {
-      type: DataTypes.ENUM('Portfolio', 'Classroom', 'IPPE', 'APPE'),
-      unique: false,
-      allowNull: false
-    },
-    reviewedBy: {
-      type: DataTypes.ENUM('PCF Only', 'APCB', 'ASC'),
-      unique: false,
-      allowNull: false
-    },
-    meetingDate: {
-      type: DataTypes.DATE,
-      unique: false,
-      allowNull: true
-    },
-    decisionRendered: {
-      type: DataTypes.STRING,
-      unique: false,
-      allowNull: true
-    }
-  });
+    const PCF = sequelize.define('pcf', {
+        pcfID: {
+            type: DataTypes.STRING,
+            unique: true,
+            primaryKey: true
+        },
+        submissionDate: {
+            type: DataTypes.DATE,
+            unique: false,
+            allowNull: false
+        },
+        submittedBy: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.ENUM('Portfolio', 'Classroom', 'IPPE', 'APPE'),
+            unique: false,
+            allowNull: false
+        },
+        reviewedBy: {
+            type: DataTypes.ENUM('PCF Only', 'APCB', 'ASC'),
+            unique: false,
+            allowNull: false
+        },
+        meetingDate: {
+            type: DataTypes.DATE,
+            unique: false,
+            allowNull: true
+        },
+        decisionRendered: {
+            type: DataTypes.STRING,
+            unique: false,
+            allowNull: true
+        }
+    });
 
-  PCF.addNewNote = async (pcf) => PCF.create({
-    ...pcf
-  });
+    PCF.addNewNote = async (pcf) => PCF.create({
+        ...pcf
+    });
 
-  return PCF;
+    return PCF;
 };
