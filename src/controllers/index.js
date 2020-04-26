ebe6a6522e53bd8c171065c54841c2b93209871f
Christopher Myers <chessmyers@aol.com>
2020-04-26 14:15:43 -0700

    Add import for general routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,6 +5,7 @@ import studentRoutes from './studentRoutes';
 import noteRoutes from './noteRoutes';
 import studentAssessmentRoutes from './studentAssessmentRoutes';
 import studentCourseRoutes from './studentCourseRoutes';
+import generalRoutes from './generalRoutes';
 
 export default {
     courseRoutes,
@@ -13,5 +14,6 @@ export default {
     studentRoutes,
     noteRoutes,
     studentAssessmentRoutes,
-    studentCourseRoutes
+    studentCourseRoutes,
+    generalRoutes
 }
