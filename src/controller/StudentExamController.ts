5b7feb8f05fcc083bd918ab7f648c54173ca895a
sophiacrennan <crennan.s@husky.neu.edu>
2020-11-08 19:12:39 -0500

    added studentExam routes and controller

--- src/controller/StudentExamController.ts
+++ src/controller/StudentExamController.ts
@@ -1,54 +1,114 @@
-import {getRepository} from "typeorm";
+import {Between, getRepository, LessThanOrEqual, MoreThanOrEqual, Raw} from "typeorm";
 import {NextFunction, Request, Response} from "express";
 import {StudentExam} from "../entity/StudentExam";
 
-export class UserController {
+export class StudentExamController {
 
     private studentExamRepository = getRepository(StudentExam);
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.studentExamRepository.find();
+    // find all studentExams
+    async all(request: Request, response: Response, next?: NextFunction) {
+        try {
+            const studentExams = await this.studentExamRepository.find();
+            await response.set({
+                'X-Total-Count': studentExams.length,
+                'Access-Control-Expose-Headers': ['X-Total-Count']
+            });
+            return studentExams;
+        } catch(e) {
+            return e;
+        }
     }
 
-    // TODO: parse query method
-    async filter(request: Request, response: Response, next: NextFunction) {
-        return this.studentExamRepository.find({
-            where: request.params.params,
-        })
-    }
+    // gets studentExams that match the given query params
+    async parseQuery(queryObj) {
+        try {
+            let where = {};
+            const paramList = Object.keys(queryObj);
 
-    async findByStudentId(request: Request, response: Response, next: NextFunction) {
-        return this.studentExamRepository.findOne({
-            where: {
-                studentId: request.params.studentId,
+            for (const param of paramList) {
+                if (param in queryObj) {
+                    let value = queryObj[param];
+
+                    switch (param) {
+                        case 'id':
+                        case 'studentId':
+                        case 'examId':
+                        //TODO: Create number values for each letterGrade for comparisons
+                        case 'letterGrade':
+                        case 'semester':
+                            where[param] = value;
+                            break;
+                        case 'year':
+                        case 'percentage':
+                            if ('max' in value && 'min' in value) {
+                                where[param] = Between(value.min, value.max);
+                            } else if ('max' in value) {
+                                where[param] = LessThanOrEqual(value.max);
+                            } else if ('min' in value) {
+                                where[param] = MoreThanOrEqual(value.min);
+                            } else if ('exact') {
+                                where[param] = value.exact;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
-        });
+            return where;
+        } catch (e) {
+            return e;
+        }
+    };
+
+    async filter(request: Request, response: Response, next?: NextFunction) {
+        try {
+            const parsedParams = await this.parseQuery(request.query);
+            const studentExams = await this.studentExamRepository.find({
+                where: parsedParams
+            });
+            await response.set({
+                'X-Total-Count': studentExams.length,
+                'Access-Control-Expose-Headers': ['X-Total-Count']
+            });
+            return studentExams;
+        } catch (e) {
+            return e;
+        }
+    };
+
+    // find a studentExam given its unique id
+    async findById(request: Request, response: Response, next?: NextFunction) {
+        try {
+            const studentExams = await this.studentExamRepository.findOne({
+                where: {id: request.params.id}
+            });
+            return studentExams;
+        } catch(e) {
+            return e;
+        }
     }
 
-    async findByExamId(request: Request, response: Response, next: NextFunction) {
-        return this.studentExamRepository.findOne({
-            where: {
-                examId: request.params.examId,
-            }
-        });
+    // Create or update an exam
+    async save(request: Request, response: Response, next?: NextFunction) {
+        try {
+            const newStudentExam = await this.studentExamRepository.save(request.body);
+            return newStudentExam;
+        } catch (e) {
+            return e;
+        }
     }
 
-    async findByIds(request: Request, response: Response, next: NextFunction) {
-        return this.studentExamRepository.findOne({
-            where: {
-                studentId: request.params.courseId,
-                examId: request.params.examId,
-            }
-        });
-    }
-
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.studentExamRepository.save(request.body);
-    }
-
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.studentExamRepository.findOne(request.params.id);
-        await this.studentExamRepository.remove(userToRemove);
+    // Delete an existing studentExam
+    async remove(request: Request, response: Response, next?: NextFunction) {
+        try {
+            const studentExamToRemove = await this.studentExamRepository.findOne(request.params.id);
+            await this.studentExamRepository.remove(studentExamToRemove);
+            return studentExamToRemove;
+        } catch (e) {
+            return e;
+        }
     }
 
 }
--- src/route/index.ts
+++ src/route/index.ts
@@ -1,7 +1,9 @@
 import {ExamRoutes} from "./ExamRoutes";
 import {UserRoutes} from "./UserRoutes";
+import {StudentExamRoutes} from "./StudentExamRoutes";
 
 export const Routes = [
     ...ExamRoutes,
+    ...StudentExamRoutes,
     ...UserRoutes,
 ]
