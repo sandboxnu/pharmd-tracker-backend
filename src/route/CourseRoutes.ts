9f5e51dca7627eade0dbf95a8c27fe2d387cb374
eco20 <eireanco@gmail.com>
2020-11-15 12:23:55 -0500

    fixed parse query exact

--- src/route/CourseRoutes.ts
+++ src/route/CourseRoutes.ts
@@ -3,15 +3,15 @@ import {CourseController} from "../controller/CourseController";
 export const CourseRoutes = [
     {
         method: "get",
-        route: "/courses",
-        controller: CourseController,
-        action: "all"
-    },
-    {
-        method: "get",
         route: "/courses/q",
         controller: CourseController,
         action: "filter"
+    },
+    {
+        method: "get",
+        route: "/courses",
+        controller: CourseController,
+        action: "all"
     },
     {
         method: "get",
