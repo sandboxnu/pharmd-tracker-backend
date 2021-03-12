ebb8d7ab8c4609a7846b43c231f9906a577e2a9c
Vera <vkong6019@gmail.com>
2021-03-12 00:40:27 -0500

    draft jest and config setup

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import {createConnection, getConnectionOptions} from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import {Routes} from './route/index';
+import {Routes} from './route';
 import {Request, Response} from "express";
 
-createConnection().then(async () => {
+const createTypeormCon = async () => {
+    const connectionOptions = await getConnectionOptions("dev");
+    await createConnection(connectionOptions).then(async () => {
 
-    // create express app
-    const app = express();
-    app.use(bodyParser.json());
+        // create express app
+        const app = express();
+        app.use(bodyParser.json());
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+        // register express routes from defined application routes
+        Routes.forEach(route => {
+            (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+                const result = (new (route.controller as any))[route.action](req, res, next);
+                if (result instanceof Promise) {
+                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
+                } else if (result !== null && result !== undefined) {
+                    res.json(result);
+                }
+            });
         });
-    });
 
-    app.listen(3000, () => {
-        console.log("Server started on port 3000! http://localhost:3000/");
-    });
-
+        app.listen(3000, () => {
+            console.log("Server started on port 3000! http://localhost:3000/");
+        });
 
 
-}).catch(error => console.log(error));
+    }).catch(error => console.log(error));
+}
+
+createTypeormCon();
