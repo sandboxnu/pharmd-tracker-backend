e2ad713919fdc1f448806446f474bf3e9f78e8aa
emaela <eireanco@gmail.com>
2021-04-08 19:07:35 -0400

    Revert "Add course to exam and studentExam queries"

--- src/controller/StudentExamController.ts
+++ src/controller/StudentExamController.ts
@@ -51,14 +51,9 @@ class StudentExamController {
     async filter(request: Request, response: Response, next?: NextFunction) {
         try {
             const parsedParams = await this.parseQuery(request.query);
-            const studentExams = await this.studentExamRepository
-                .createQueryBuilder('studentExam')
-                .where(parsedParams)
-                .leftJoinAndSelect("studentExam.student", "student")
-                .leftJoinAndSelect("studentExam.exam", "exam")
-                .leftJoinAndSelect("exam.course", "course")
-                .getMany()
-            ;
+            const studentExams = await this.studentExamRepository.find({
+                where: parsedParams,
+            });
             await response.set({
                 'X-Total-Count': studentExams.length,
                 'Access-Control-Expose-Headers': ['X-Total-Count'],
@@ -72,15 +67,10 @@ class StudentExamController {
     // find a studentExam given its unique id
     async findById(request: Request, response: Response, next?: NextFunction) {
         try {
-            return await this.studentExamRepository
-                .createQueryBuilder('studentExam')
-                .where({id: request.params.id})
-                .leftJoinAndSelect("studentExam.student", "student")
-                .leftJoinAndSelect("studentExam.exam", "exam")
-                .leftJoinAndSelect("exam.course", "course")
-                .getOne()
-            ;
-        } catch(e) {
+            return await this.studentExamRepository.findOne({
+                where: { id: request.params.id },
+            });
+        } catch (e) {
             return e;
         }
     }
